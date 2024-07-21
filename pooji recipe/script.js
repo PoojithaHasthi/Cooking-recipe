931f9114cb06444cd42c88d9400b9d3a67323aac
PoojithaHasthi <147254988+PoojithaHasthi@users.noreply.github.com>
2024-07-21 12:04:05 +0530

    Add files via upload

---
+++ pooji recipe/script.js
@@ -0,0 +1,81 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove("showRecipe");
+});
+
+function getMealList() {
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if (data.meals) {
+            data.meals.forEach(meal => {
+                html += `
+                    <div class="meal-item" data-id="${meal.idMeal}">
+                        <div class="meal-img">
+                            <img src="${meal.strMealThumb}" alt="food" height="300" width="300"> 
+                        </div>
+                        <div class="meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href="#" class="recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else {
+            html = "Sorry! we didn't find any meal.";
+            mealList.classList.add('notFound');
+        }
+        mealList.innerHTML = html;
+    });
+}
+
+function getMealRecipe(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('recipe-btn')) {
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+}
+
+function mealRecipeModal(meal) {
+    meal = meal[0];
+    let html = `
+        <h2 class="recipe-title">${meal.strMeal}</h2>
+        <p class="recipe-category">${meal.strCategory}</p>
+        <div class="recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class="recipe-meal-img">
+            <img src="${meal.strMealThumb}" alt="">
+        </div>
+        <div class="recipe-link">
+            <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+        </div>
+    `;
+    if (meal.strYoutube) {
+        html += `
+            <div class="recipe-video">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/${getYoutubeId(meal.strYoutube)}" frameborder="0" allowfullscreen></iframe>
+            </div>
+        `;
+    }
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
+}
+
+function getYoutubeId(url) {
+    const match = url.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/);
+    return match && match[1];
+}
